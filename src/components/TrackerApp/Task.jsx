38766f7ad3d40d94437f7246d6eb68c89b3d13a0
Bilal Ahmed <55283639+Bilal05476@users.noreply.github.com>
2021-06-19 20:41:24 +0500

    updates

--- src/components/TrackerApp/Task.jsx
+++ src/components/TrackerApp/Task.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Task = ({ task }) => {
+  const [value , onChange] = useState(new Date());
   return (
     <div>
       <h1>{task.name}</h1>
